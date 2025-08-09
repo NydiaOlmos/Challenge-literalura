3a18b355bdc723fd1e6d60cc4f31ea7ef1bbaae8
NydiaOlmos <nydia.naomi@gamil.com>
2025-08-08 18:16:52 -0600

    Versión final

--- src/main/java/com/naomi/literalura/repository/AutorRepository.java
+++ src/main/java/com/naomi/literalura/repository/AutorRepository.java
@@ -15,6 +15,8 @@ public interface AutorRepository extends JpaRepository<Autor, Integer> {
     @Query(value = "SELECT a FROM Autor a WHERE a.fechaNacimiento <= :epoca AND a.fechaDefuncion >= :epoca")
     List<Autor> filtrarPorEpoca(Integer epoca);
 
+
+    //  Filtra según la cantidad de libros que un autor ha escrito un libro
     @Query(value = "SELECT a FROM Autor a JOIN a.libros l GROUP BY a HAVING COUNT(l) >= :minimo")
     List<Autor> busquedaMinimoLibros(Integer minimo);
 }
